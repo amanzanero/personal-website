2cd30932840e0e57cf04bbab547d43cd39b65065
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-11-20 08:30:31 -0800

    remove logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 import Headroom from 'react-headroom';
-import { Flex, Image } from 'rebass';
+import { Flex } from 'rebass';
 import styled from 'styled-components';
 import { SectionLinks } from 'react-scroll-section';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
-import Logo from './Logo/Portfolio.svg';
 
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
 
@@ -21,16 +20,10 @@ const HeaderContainer = styled(Headroom)`
 const formatLinks = allLinks =>
   Object.entries(allLinks).reduce(
     (acc, [key, value]) => {
-      const isHome = key === 'home';
-      return isHome
-        ? {
-            ...acc,
-            home: value,
-          }
-        : {
-            ...acc,
-            links: [...acc.links, { name: capitalize(key), value }],
-          };
+      return {
+        ...acc,
+        links: [...acc.links, { name: capitalize(key), value }],
+      };
     },
     { links: [], home: null },
   );
@@ -40,25 +33,14 @@ const Header = () => (
     <Fade top>
       <Flex
         flexWrap="wrap"
-        justifyContent="space-between"
+        justifyContent="flex-start"
         alignItems="center"
         p={3}
       >
         <SectionLinks>
           {({ allLinks }) => {
-            const { home, links } = formatLinks(allLinks);
+            const { links } = formatLinks(allLinks);
 
-            const homeLink = home && (
-              <Image
-                src={Logo}
-                width="50px"
-                alt="Portfolio Logo"
-                onClick={home.onClick}
-                style={{
-                  cursor: 'pointer',
-                }}
-              />
-            );
             const navLinks = links.map(({ name, value }) => (
               <RouteLink
                 key={name}
@@ -70,7 +52,6 @@ const Header = () => (
 
             return (
               <Fragment>
-                {homeLink}
                 <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
               </Fragment>
             );
